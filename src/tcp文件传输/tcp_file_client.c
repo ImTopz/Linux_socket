edcb3146a96353ac6923992bc7c4fcfe45e9318f
ImTopz <1976521709@qq.com>
2023-03-21 13:12:33 +0800

    Init

---
+++ src/tcp文件传输/tcp_file_client.c
@@ -0,0 +1,91 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define CLIENT_PORT 1234
+
+char filename[100];
+char buff[1024];
+char buff2[10] = "##########";
+
+int TransFile(int fd)
+{
+    
+    while(1){
+    printf("请输入文件名，长度请在100以内！\n");
+    scanf("%s",filename);
+    FILE *fp = fopen(filename,"r");
+    if(fp == NULL)
+    {
+        printf("文件不存在！\n");
+      
+    }
+    printf("开始进行传输!\n");
+    
+    send(fd,filename,sizeof(filename),0);
+    int tmp_buff = 0;
+    while( (tmp_buff = fread(buff,sizeof(char),1024,fp)) > 0 )
+    {
+        send(fd,buff,tmp_buff,0);
+    }
+     send(fd,buff2,sizeof(buff2),0);
+    
+    fclose(fp);
+    printf("传输完毕！\n");
+
+    }
+    return 0;
+
+}
+
+
+int InitTCP()
+{   
+    printf("fd initing!\n");
+    int client_fd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    printf("%d\n",client_fd);
+    if(client_fd < 0)
+    {
+        printf("建立套接字失败!\n");
+        return -1;
+    }
+    printf("create success!");
+    struct sockaddr_in server_addr;
+    memset(&server_addr,0,sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(CLIENT_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   
+    int conn = connect(client_fd,(struct sockaddr*)&server_addr,sizeof(server_addr));
+    if(conn < 0)
+    {
+        printf("建立连接失败！\n");
+        return -2;
+    }
+
+    TransFile(client_fd);
+
+    
+ 
+    close(conn);
+    
+    close(client_fd);
+    return client_fd;    
+}
+
+
+
+
+
+
+int main(int argc,char *argv[])
+{
+  printf("client starting!\n");
+  int client_fd = InitTCP();
+  return 0;
+
+}
---
+++ src/tcp文件传输/tcp_file_server.c
@@ -0,0 +1,100 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+
+
+#define SERVER_PORT 1234
+#define BUFF_LEN 512
+
+char filename[100];
+char buff[1024];
+char buff2[1];
+
+int InitTCP()
+{
+    int server_fd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    if(server_fd < 0)
+    {
+        printf("建立tcp套接字失败！");
+        return -1;
+    }
+    struct sockaddr_in server_addr;
+    memset(&server_addr,0,sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(server_fd,(struct sockaddr*)&server_addr,sizeof(server_addr))<0)
+    {
+        printf("bind error!");
+        return -2;
+    }
+    if(listen(server_fd,SOMAXCONN)<0)
+    {
+        printf("listen error!");
+        return -3;
+    }
+    return server_fd;
+
+}
+
+int InitAccp(int fd){
+    struct sockaddr_in client_addr;
+    memset(&client_addr,0,sizeof(client_addr));
+    socklen_t client_len = sizeof(client_addr);
+    int conn = 0;
+    if((conn = accept(fd,(struct sockaddr*)&client_addr,&client_len))<0)
+    {
+        printf("accept error!\n");
+        return -3;
+    }
+    printf("connect success!\n");
+        
+    while(1){
+    recv(conn,filename,sizeof(filename),0);
+    FILE *fp = fopen(filename,"w");
+    if(fp == NULL)
+    {
+    	printf("not exits!");
+        return -1;
+    }
+    printf("开始传送！\n");
+    int temp_buffer = 0;
+        while((temp_buffer = recv(conn,buff,sizeof(buff),0))>0 ){
+            int cnt = 0;
+            for(int i =0;i<strlen(buff);i++)
+            {
+             if(buff[i]=='#')
+             {
+                 cnt ++;
+             }
+            }
+            if(cnt >= 5)
+                break;
+            fwrite(buff, sizeof(char), temp_buffer, fp);
+        }
+    printf("传送完毕！\n");
+    fclose(fp);
+    }
+    close(conn);
+    close(fd);
+        
+   
+}
+
+int main(int argc,char *argv[])
+{
+  
+   printf("welcome to tcp file transform!\n");
+   printf("开始创建套接字并进行监听！\n");
+   int sock_fd = InitTCP();
+   printf("starting AC!\n");
+   InitAccp(sock_fd);
+   
+
+   
+
+
+}
